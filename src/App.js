20da69a478341dcd78c84725788f74a6436e28ec
Edward Stevens <edwardstevens@Edwards-MacBook-Pro.local>
2019-10-16 18:12:21 +0700

    changing bootstrap postion in file for netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import CityButtons from './components/CityButtons';
 import { cities } from "./utils";
